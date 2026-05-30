3a98f615e1120943e5776a7f913e782cd6dab793
m-epasta <m-epasta@github.com>
2026-05-30 23:12:04 +0200

    refactor: move out map (unused & unimplemented) strip out constants from unnecessary complexity and add an string array variant to contains
    delete gen_clicodes.sh because it introduced (with other files also)
    *bad* complexity that led to messy structure

--- include/array_helpers.h
+++ include/array_helpers.h
@@ -1,14 +1,25 @@
 #ifndef ARRAY_HELPERS_H
 #define ARRAY_HELPERS_H
 
+#include <string.h>
+
 #include "vector.h"
 
-int contains(vector_str* arr, char* string) {
+int vcontains(vector_str* arr, const char* string) {
     for (size_t i = 0; i <= arr->size; ++i) {
         if (strcmp(vector_item(*arr, i), string) == 0) return 0;
     }
 
     return 1;
+}
+
+char* acontains(const char* arr[], const char* string, size_t size) {
+    for (size_t i = 0; i <= size; ++i) {
+        const char* curr = arr[i];
+        if (strcmp(curr, string) == 0) return curr;
+    }
+
+    return NULL;
 }
 
 #endif  // !ARRAY_HELPERS_H
--- include/map.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAP_H
-#define MAP_H
-
-#include <stdlib.h>
-#include <string.h>
-
-#endif  // !MAP_H
